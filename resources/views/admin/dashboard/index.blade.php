6761ab3eec8600aa36058adc61650f4ee07e6c7b
Hadyan Ahmad <hadyandev@gmail.com>
2020-05-19 14:33:16 +0700

    02. Integrasi dengan Sleek Admin Dashboard

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layout')
+
+@section('content')
+    <h1>Page Dashboard</h1>    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::get('dashboard', 'DashboardController@index');
+});
